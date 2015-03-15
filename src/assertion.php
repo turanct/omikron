e59b7e7d4dd6ebb03f90c7d34e1eb5585393ed7a
turanct <spinnewebber_toon@hotmail.com>
2015-03-15 15:36:47 +0100

    Add assertion that checks if no exceptions were thrown

--- src/assertion.php
+++ src/assertion.php
@@ -21,6 +21,16 @@ function expectToThrow(callable $expression, $exception = null) {
     return "Expected " . ($exception ?: 'Exception') . " to be thrown\n";
 }
 
+function expectNotToThrow(callable $expression) {
+    try {
+        $expression();
+    } catch (Exception $e) {
+        return "Unexpected exception was thrown: [" . get_class($e) . "] " . $e->getMessage() . "\n";
+    }
+
+    return true;
+}
+
 function diff($actual, $expected) {
     // workaround for bug in differ https://github.com/sebastianbergmann/diff/issues/25
     $actual = is_scalar($actual) ? (string) $actual : $actual;
