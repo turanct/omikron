96010a9e261bd8b482a31e9ffa5fde421909b51e
turanct <spinnewebber_toon@hotmail.com>
2015-08-30 23:37:08 +0200

    Catch exceptions in assertions
    - Catch exceptions, display stack trace
    - Add regression test (in fact it's an integration test again...)

---
+++ tests/topic-exceptions.php
@@ -0,0 +1,49 @@
+<?php
+
+function bad()
+{
+    throw new Exception('Something');
+}
+
+$sampleTopic = within("foo",
+    describe("bar",
+        it("throws an exception", function() { return
+            expect(bad(), toBe('good'));
+        })
+    )
+);
+
+$expectedOutput = <<<OUTPUT
+topics: 1
+features: 1
+assertions: 1
+
+FAILED: foo: bar throws an exception
+
+Exception: Something in /vagrant/tests/topic-exceptions.php on line 5
+
+Call stack:
+#0 /vagrant/tests/topic-exceptions.php(11): bad()
+#1 /vagrant/src/omikron.php(51): {closure}()
+#2 /vagrant/tests/topic-exceptions.php(12): it('throws an excep...', Object(Closure))
+#3 /vagrant/src/input.php(37): include('/vagrant/tests/...')
+#4 [internal function]: {closure}('/vagrant/tests/...')
+#5 /vagrant/src/input.php(40): array_map(Object(Closure), Array)
+#6 /vagrant/bin/omikron(30): getTopics(Array)
+#7 {main}
+
+OUTPUT;
+
+return within("omikron",
+    describe("assertion",
+        it("catches exceptions in callable", function() use ($sampleTopic, $expectedOutput) {
+            try {
+                $renderedOutput = renderOutput(testResults([$sampleTopic]));
+            } catch (Exception $e) {
+                $renderedOutput = '';
+            }
+
+            return expect($renderedOutput, toBe($expectedOutput));
+        })
+    )
+);
